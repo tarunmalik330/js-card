7e3a9f39aaa8a91f3e42cf82d909a12b2cc94a7e
tarunmalik330 <maliktarun2005@gmail.com>
2023-10-09 11:44:17 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+var card = [
+
+    {
+        Img: './assets/images/img1.png',
+        h2: 'Everyday Things are',
+        heading2: ' Colorful',
+        p: 'Lorem ipsum dolor sit amet consectetur. Elementum quam blandit fusce laoreet nunc lectus',
+    },
+    {
+        Img: './assets/images/img2.png',
+        h2: 'Everyday Things are',
+        heading2: ' Colorful',
+        p: 'Lorem ipsum dolor sit amet consectetur. Elementum quam blandit fusce laoreet nunc lectus',
+    },
+    {
+        Img: './assets/images/img3.png',
+        h2: 'Everyday Things are',
+        heading2: ' Colorful',
+        p: 'Lorem ipsum dolor sit amet consectetur. Elementum quam blandit fusce laoreet nunc lectus',
+    },
+
+]
+const mycontainer = document.querySelector('.cardcontainer');
+const mycard =
+    card.map((postdata) => {
+        return `
+    <div class="card">
+    <img src=${postdata.Img} alt="">
+<h2 class="card_heading">${postdata.h2}<span class="colorful">${postdata.heading2}</span></h2>
+    <p>${postdata.p}</p>
+
+    </div>
+    `
+    }).join(" ")
+mycontainer.innerHTML = mycard;
